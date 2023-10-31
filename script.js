12ebb84c114e12a1cd880575a9821c7ff38cc74c
Earl Angelo Dumagan <149300528+dumaganearl@users.noreply.github.com>
2023-10-31 16:19:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text", {
+    strings: ["Network Administrator", "XXX", "Aspirant Trader"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
